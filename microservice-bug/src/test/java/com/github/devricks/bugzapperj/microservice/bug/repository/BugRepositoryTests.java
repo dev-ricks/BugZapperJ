f442244ebfa40fc937020eebd9add8fee25a2164
dev-ricks <richard.smit.dev@gmail.com>
2024-09-20 00:40:23 -0400

    chore: Turn off show sql setting.
    Set show sql setting for application and tests to false.
    
    GitHub

--- microservice-bug/src/test/java/com/github/devricks/bugzapperj/microservice/bug/repository/BugRepositoryTests.java
+++ microservice-bug/src/test/java/com/github/devricks/bugzapperj/microservice/bug/repository/BugRepositoryTests.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @PropertyMapping("spring")
-@DataJpaTest
+@DataJpaTest(showSql = false)
 class BugRepositoryTests {
 
     private final BugRepository bugRepository;
